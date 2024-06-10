b110c9b753368f71b37ce554e7e2d2fd1a683c9c
m1ca000 <164351016+m1ca000@users.noreply.github.com>
2024-06-10 15:33:07 -0300

    InteractionArea, tomar alimento complete

---
+++ Assets/InteractionAreaScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionAreaScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void alimentarse(int ValorAlimentario)
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        FileteScript filete;
+        filete = other.GetComponent<FileteScript>();
+        if (filete)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
